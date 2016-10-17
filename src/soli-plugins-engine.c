8073be3cb9954bd39ace833e149a2c50b8f72659
David Luco <dluco11@gmail.com>
2016-10-16 17:15:03 -0700

    Fix cmdline option visibility.

--- src/soli-plugins-engine.c
+++ src/soli-plugins-engine.c
@@ -1,25 +1,30 @@
-/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
 /*
  * soli-plugins-engine.c
- * Copyright (C) 2016 David Luco <dluco11@gmail.com>
+ * This file is part of soli
  *
- * soli is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or
+ * Copyright (C) 2002-2005 Paolo Maggi
+ * Copyright (C) 2010 Steve Frécinaux
+ * Copyright (C) 2016 David Luco
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * soli is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "soli-plugins-engine.h"
 
+#include <glib.h>
 #include <girepository.h>
+#include "soli-debug.h"
 #include "soli-dirs.h"
 #include "soli-settings.h"
 
@@ -35,10 +40,64 @@ G_DEFINE_TYPE (SoliPluginsEngine, soli_plugins_engine, PEAS_TYPE_ENGINE)
 static SoliPluginsEngine *default_engine = NULL;
 
 static void
+soli_plugins_engine_pre_load_plugin (PeasEngine		*engine,
+									 PeasPluginInfo *info,
+									 gpointer		user_data)
+{
+	const gchar *module_dirname;
+
+	module_dirname = peas_plugin_info_get_module_dir (info);
+
+	if (g_str_has_prefix (module_dirname, soli_dirs_get_user_plugins_dir ()))
+	{
+		GDir *module_dir;
+		const gchar *filename;
+		gboolean has_schema = FALSE;
+
+		module_dir = g_dir_open (module_dirname,
+								 0, NULL);
+
+		if (module_dir == NULL)
+		{
+			return;
+		}
+
+		/* Search (non-recursively) in plugin module dir for a schema file */
+		while ((filename = g_dir_read_name (module_dir)))
+		{
+			if (g_str_has_suffix (filename, "gschema.xml"))
+			{
+				has_schema = TRUE;
+				break;
+			}
+		}
+
+		g_dir_close (module_dir);
+
+		if (has_schema)
+		{
+			/*
+			 * Force compilation of plugin's schema file.
+			 *
+			 * This is done before the plugin is loaded so that its
+			 * settings are available immediately.
+			 * */
+			GSettings *settings = peas_plugin_info_get_settings (info, NULL);
+			if (settings != NULL)
+			{
+				g_clear_object(&settings);
+			}
+		}
+	}
+}
+
+static void
 soli_plugins_engine_init (SoliPluginsEngine *engine)
 {
 	gchar *typelib_dir;
 	GError *error = NULL;
+
+	soli_debug (DEBUG_PLUGINS);
 
 	peas_engine_enable_loader (PEAS_ENGINE (engine), "python3");
 
@@ -77,18 +136,23 @@ soli_plugins_engine_init (SoliPluginsEngine *engine)
 	}
 
 	peas_engine_add_search_path (PEAS_ENGINE (engine),
-								soli_dirs_get_user_plugins_dir (),
-								soli_dirs_get_user_plugins_dir ());
+								 soli_dirs_get_user_plugins_dir (),
+								 soli_dirs_get_user_plugins_dir ());
 
 	peas_engine_add_search_path (PEAS_ENGINE (engine),
-								soli_dirs_get_soli_plugins_dir (),
-								soli_dirs_get_soli_plugins_data_dir ());
+								 soli_dirs_get_soli_plugins_dir (),
+								 soli_dirs_get_soli_plugins_data_dir ());
+
+	g_signal_connect(PEAS_ENGINE (engine),
+					 "load-plugin",
+					 G_CALLBACK (soli_plugins_engine_pre_load_plugin),
+					 NULL);
 
 	g_settings_bind (engine->plugin_settings,
-					SOLI_SETTINGS_ACTIVE_PLUGINS,
-					engine,
-					"loaded-plugins",
-					G_SETTINGS_BIND_DEFAULT);
+					 SOLI_SETTINGS_ACTIVE_PLUGINS,
+					 engine,
+					 "loaded-plugins",
+					 G_SETTINGS_BIND_DEFAULT);
 }
 
 static void
@@ -118,7 +182,7 @@ soli_plugins_engine_get_default (void)
 															NULL));
 
 		g_object_add_weak_pointer (G_OBJECT (default_engine),
-									(gpointer) &default_engine);
+								   (gpointer) &default_engine);
 	}
 
 	return default_engine;
--- src/soli.c
+++ src/soli.c
@@ -1,64 +1,75 @@
-/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
 /*
- * main.c
- * Copyright (C) 2016 David Luco <dluco11@gmail.com>
+ * soli.c
+ * This file is part of soli
  *
- * Soli is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or
+ * Copyright (C) 2005 - Paolo Maggi
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
- * Soli is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
 
-#include <gtk/gtk.h>
-#include <girepository.h>
-
+#include "soli-app.h"
 #include "soli-app-x11.h"
+
+#include <glib.h>
+#include <locale.h>
+
 #include "soli-dirs.h"
+#include "soli-debug.h"
 
 int
 main (int argc, char *argv[])
 {
 	SoliApp *app;
-	GOptionContext *context;
-	GError *error = NULL;
 	gint status;
+	const gchar *dir;
 
-	context = g_option_context_new (NULL);
-	g_option_context_add_group (context, g_irepository_get_option_group ());
 
-	g_option_context_parse (context, &argc, &argv, &error);
-	if (error != NULL)
-	{
-		g_print ("%s: %s\n", PACKAGE, error->message);
-		g_error_free (error);
-		error = NULL;
-	}
-	g_option_context_free (context);
-
+	/* NOTE: we should not make any calls to the soli api before the
+	 * private library is loaded */
 	soli_dirs_init ();
-	
+
+	/* Setup locale */
+	setlocale (LC_ALL, "");
+
+	dir = soli_dirs_get_soli_locale_dir ();
+
 	app = g_object_new (SOLI_TYPE_APP_X11,
-						"application-id", "ca.dluco.soli",
-						"flags", G_APPLICATION_HANDLES_COMMAND_LINE | G_APPLICATION_HANDLES_OPEN,
-						NULL);
+	                    "application-id", "ca.dluco.soli",
+	                    "flags", G_APPLICATION_HANDLES_COMMAND_LINE | G_APPLICATION_HANDLES_OPEN,
+	                    NULL);
 
 	status = g_application_run (G_APPLICATION (app), argc, argv);
 
+	/* Break reference cycles caused by the PeasExtensionSet
+	 * for soliAppActivatable which holds a ref on the soliApp
+	 */
 	g_object_run_dispose (G_OBJECT (app));
 
+	g_object_add_weak_pointer (G_OBJECT (app), (gpointer *) &app);
 	g_object_unref (app);
+
+	if (app != NULL)
+	{
+		soli_debug_message (DEBUG_APP, "Leaking with %i refs",
+		                     G_OBJECT (app)->ref_count);
+	}
 
 	return status;
 }
+
+/* ex:set ts=8 noet: */
